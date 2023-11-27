b4bef6e5b8d1f9b699715387102cfa26d02ea844
kallealpeus <alpeusk@gmail.com>
2023-11-27 23:43:19 +0100

    Knappar för bakgrundsfärger

--- my-app/src/Components/SettingsPage/Settings.js
+++ my-app/src/Components/SettingsPage/Settings.js
@@ -1,9 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import './Settings.css'
 
 const Settings = () => {
-    return(
+    const changeColor = () => {
+        const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        document.body.style.backgroundColor = randomColor;
+      };
+    const [isBlack, setIsBlack] = useState(false);
+        
+    const toggleColor = () => {
+        setIsBlack((prevIsBlack) => !prevIsBlack);
+        document.body.style.backgroundColor = isBlack ? 'white' : 'black';
+    };
+
+
+  return ( 
+    <div id='settingsContainer'>
         <h1>Settings</h1>
-    );
-}
+        <div>
+            <input type='checkbox' id='colorToggle' onChange={toggleColor}/>
+            <label for="colorToggle">Darkmode</label>
+        </div>
+        <button onClick={changeColor}>
+            Generate Random Color
+        </button>
+        
+    </div>
+  );
+};
 
 export default Settings;
