378834ea02c03f785674b5dcb71e1eedc4c0e015
timchristoffer <timdaahl@gmail.com>
2023-11-27 13:15:37 +0100

    Skapade en profilbildsgenererare

--- my-app/src/Components/profile/profile.js
+++ my-app/src/Components/profile/profile.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './profile.css';
+import { useUser } from '../../UserContext';
+import { getRandomProfilePic } from '../LoginSignup/LoginSignup.jsx';
 
 const ProfilePage = () => {
+  const { username, email, setUser } = useUser();
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    if (storedEmail) {
+      setUser(username, storedEmail);
+    }
+  }, [username, setUser]);
+
+  // Use getRandomProfilePic() to get a random profile picture
+  const profilePic = getRandomProfilePic();
+
   return (
     <div className="profile-page-container">
-      <div className="profile-page-header">
-        <h1>User Profile</h1>
-      </div>
-        <div className="profile-page-content">
+      <div className="profile-page-content">
         <div className="profile-page-picture">
-          {/* Include user profile picture here */}
-          <img src="profile-picture.jpg" alt="User Profile" />
+          {/* Use the random profile picture */}
+          <img src={profilePic} alt="User Profile" />
         </div>
-            <div className="profile-page-details">
-          <h2>Hussen Abdullahi</h2>
-          <p>Email: Hussen@example.com</p>
+        <div className="profile-page-details">
+          <h2>User</h2>
+          <h3>{username}</h3>
+          <h2>Email: </h2>
+          <h3>{email}</h3>
         </div>
       </div>
     </div>
--- my-app/src/UserContext.js
+++ my-app/src/UserContext.js
@@ -1,16 +1,28 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
 
-  const setUser = (newUsername) => {
+  useEffect(() => {
+    // Fetch email from localStorage when the component mounts
+    const storedEmail = localStorage.getItem('email');
+    if (storedEmail) {
+      setEmail(storedEmail);
+    }
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+  const setUser = (newUsername, newEmail) => {
     setUsername(newUsername);
+    setEmail(newEmail);
+    // Store email in localStorage
+    localStorage.setItem('email', newEmail);
   };
 
   return (
-    <UserContext.Provider value={{ username, setUser }}>
+    <UserContext.Provider value={{ username, email, setUser }}>
       {children}
     </UserContext.Provider>
   );
